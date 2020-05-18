8c60d417424dd21670fe257f0c519b67153d0eaa
WFRousseau <williamrousseau14@gmail.com>
2020-05-18 15:57:03 +0200

    TD-14-dev(projectiles): projectile class updated

--- src/models/Projectile.java
+++ src/models/Projectile.java
@@ -1,30 +1,61 @@
 package models;
 
-public class Projectile {
+import java.util.ArrayList;
 
-    private Location location;
+public abstract class Projectile extends Entity {
+
     private Location destination;
-    private Location direction;
-    private Entity sender;
+    private Vector direction;
+    private Node sender;
     private int damage;
 
-    public Projectile(Location location, Location destination, Location direction, Entity sender, int damage) {
-        this.location = location;
+    public Projectile(Location location, Location destination, Vector direction, Node sender, int damage, int range) {
+        super(range, location);
         this.destination = destination;
         this.direction = direction;
         this.sender = sender;
         this.damage = damage;
     }
 
-    /*public boolean inRange() {
-
+    public Projectile(Location location, Node sender, int damage, int range) {
+        super(range, location);
+        this.sender = sender;
+        this.damage = damage;
     }
 
-    public void hit() {
+    public abstract void hit(ArrayList<Virus> inRangeVirus);
 
-    }*/
+    public Location getDestination() {
+        return this.destination;
+    }
 
-    public Location getLocation() {
-        return this.location;
+    public Vector getDirection() {
+        return this.direction;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public Entity getSender() {
+        return this.sender;
+    }
+
+    public ArrayList<Virus> getInRangeVirus() {
+        return this.sender.getInRangeVirus();
+    }
+
+    public boolean isInRange(Virus virus){
+        int nearestX = Math.max(virus.getLocation().getCol(), Math.min(this.getLocation().getCol(),virus.getLocation().getCol()+virus.getSizeW()));
+        int nearestY = Math.max(virus.getLocation().getRow(), Math.min(this.getLocation().getRow(),virus.getLocation().getRow()+virus.getSizeH()));
+
+        int deltaX = this.getLocation().getCol() - Math.max(virus.getLocation().getCol(), Math.min(this.getLocation().getCol(),virus.getLocation().getCol()+virus.getSizeW()));
+        int deltaY = this.getLocation().getRow() - Math.max(virus.getLocation().getRow(), Math.min(this.getLocation().getRow(),virus.getLocation().getRow()+virus.getSizeH()));
+
+        return (Math.pow(deltaX,2) + Math.pow(deltaY,2)) < Math.pow(this.getRange(),2);
+    }
+
+    public void explode(Virus virus) {
+        virus.setCurrentHP(virus.getCurrentHP()-this.getDamage());
     }
 }

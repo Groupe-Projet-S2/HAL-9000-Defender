8b953f9d70a3403112abc316f43188db7bbf8a25
WFRousseau <williamrousseau14@gmail.com>
2020-05-18 16:03:47 +0200

    TD-16-dev(static-projectiles): create static class

--- src/models/Static.java
+++ src/models/Static.java
@@ -1,11 +1,20 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Static extends Projectile {
 
-    private int range;
+    public Static(Location location, Location destination, Vector direction, Node sender, int damage, int range) {
+        super(location, destination, direction, sender, damage, range);
+    }
 
-    public Static(Location location, Location destination, Location direction, Entity sender, int damage, int range) {
-        super(location, destination, direction, sender, damage);
-        this.range = range;
+    public void hit(ArrayList<Virus> inRangeVirus) {
+
+        for (int i = 0; i < inRangeVirus.size(); i++) {
+            if (this.isInRange(inRangeVirus.get(i))) {
+                this.explode(inRangeVirus.get(i));
+            }
+
+        }
     }
 }

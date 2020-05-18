261a7b4d6cc316239de0dd5816a41195adef4f9c
WFRousseau <williamrousseau14@gmail.com>
2020-05-18 15:42:58 +0200

    TD-13-dev(sprint2): created Vector class

--- src/models/Vector.java
+++ src/models/Vector.java
@@ -1,5 +1,30 @@
 package models;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Vector {
 
+    private int row, col;
+
+    public Vector(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
 }

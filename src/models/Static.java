445a9b94a114d6b642418dc5b49ae7e63661285d
WFRousseau <williamrousseau14@gmail.com>
2020-05-21 02:04:50 +0200

    TD-21-dev(static-projectiles): update static class

--- src/models/Static.java
+++ src/models/Static.java
@@ -8,13 +8,43 @@ public class Static extends Projectile {
         super(location, destination, direction, sender, damage, range);
     }
 
-    public void hit(ArrayList<Virus> inRangeVirus) {
-
-        for (int i = 0; i < inRangeVirus.size(); i++) {
-            if (this.isInRange(inRangeVirus.get(i))) {
-                this.explode(inRangeVirus.get(i));
+    private int isInTheWay() {
+        for (int i = 0 ; i < this.getInRangeVirus().size() ; i++) {
+            if (this.isInRange(this.getInRangeVirus().get(i))) {
+                return i;
             }
+        }
+        return -1;
+    }
 
+    @Override
+    public void move() {
+        if (this.isInTheWay()=-1 || !this.location.match(this.destination)) {
+            if(this.location.getRow()!=this.destination.getRow()) {
+                if(this.location.getRow()<this.destination.getLocation().getRow()) {
+                    this.direction.setRow(1);
+                }
+                else {
+                    this.direction.setRow(-1);
+                }
+            }
+            if(this.location.getCol()!=this.destination.getLocation().getCol()) {
+                if(this.location.getCol()<this.destination.getLocation().getCol()) {
+                    this.direction.setCol(1);
+                }
+                else {
+                    this.direction.setCol(-1);
+                }
+            }
+            this.location.setRow(this.location.getRow()+this.direction.getRow());
+            this.location.setCol(this.location.getCol()+this.direction.getCol());
+        }
+    }
+
+    @Override
+    public void hit(ArrayList<Virus> inRangeVirus) {
+        if (this.isInTheWay()!=-1) {
+            this.explode(inRangeVirus.get(this.isInTheWay()));
         }
     }
 }

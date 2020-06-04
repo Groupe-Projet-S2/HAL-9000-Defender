bf02c99af9ee94b75350fe6e8729f919ca3bf355
Cedric-F <cedric.fontaine@gmx.fr>
2020-06-04 17:54:18 +0200

    TD-31-dev(ransomware): Ransom pop up

--- src/models/entities/Ransomware.java
+++ src/models/entities/Ransomware.java
@@ -1,16 +1,30 @@
 package models.entities;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.layout.VBox;
 import models.environment.Location;
 import models.environment.Tile;
+import views.AlertBox;
 
 public class Ransomware extends Virus {
 
     private Location target;
     private boolean triggered;
+    private ObservableList<VBox> ransoms;
     public Ransomware(Location location, Tile tile, Location target) {
         super(0, location, tile, 2);
         this.target = target;
         this.triggered = false;
+        this.ransoms = FXCollections.observableArrayList();
+    }
+
+    public ObservableList<VBox> getRansoms() {
+        return this.ransoms;
+    }
+
+    public void close() {
+        ransoms.clear();
     }
 
     @Override
@@ -22,7 +36,9 @@ public class Ransomware extends Virus {
     public void act() {
         if (location.match(target) && ! triggered) {
             triggered = true;
-            System.out.println("Hello");
+            AlertBox box = new AlertBox(this);
+            VBox ransom = box.ransom();
+            ransoms.add(ransom);
         }
     }
 }
--- src/views/AlertBox.java
+++ src/views/AlertBox.java
@@ -3,16 +3,17 @@ package views;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import models.entities.Adware;
+import models.entities.Ransomware;
+import models.entities.Virus;
 import models.environment.Location;
 
 public class AlertBox {
 
-    private Adware adware;
-    public AlertBox(Adware adware) {
-        this.adware = adware;
+    private Virus virus;
+    public AlertBox(Virus virus) {
+        this.virus = virus;
     }
 
     public VBox popUp(Location location) {
@@ -28,8 +29,25 @@ public class AlertBox {
         //layout.styleProperty().setValue("-fx-border: black");
         layout.setOpacity(0.5);
         Button close = new Button("OK");
-        close.setOnAction(e -> adware.close(layout));
+        close.setOnAction(e -> ((Adware) virus).close(layout));
         layout.getChildren().addAll(title, close);
+        return layout;
+    }
+
+    public VBox ransom() {
+        VBox layout = new VBox();
+        Label title = new Label("Pay now if you want to live.");
+        title.styleProperty().setValue("-fx-text-fill: #ffffff");
+        layout.setAlignment(Pos.CENTER);
+        layout.setLayoutX(0);
+        layout.setLayoutY(0);
+        layout.setPrefHeight(800);
+        layout.setPrefWidth(800);
+        layout.styleProperty().setValue("-fx-background-color: #0000FF");
+        layout.setOpacity(0.7);
+        Button pay = new Button("Pay");
+        pay.setOnAction(e -> ((Ransomware) virus).close());
+        layout.getChildren().addAll(title, pay);
         return layout;
     }
 }

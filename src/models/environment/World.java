a4f2efff53ababf22df0244fd606381e9ea93992
WFRousseau <williamrousseau14@gmail.com>
2020-06-05 11:18:57 +0200

    TD-32-refactor(tower): moving methods

--- src/models/environment/World.java
+++ src/models/environment/World.java
@@ -50,7 +50,6 @@ public class World {
         return virusList;
     }
 
-
     public ArrayList<Projectile> getProjectileList(){
         ArrayList<Projectile> projectileList = new ArrayList<>();
         // projectileList.addAll((Collection<? extends Projectile>) entities.stream().filter(entity -> Entity.isProjectile(entity)).collect(Collectors.toList()));
@@ -84,68 +83,22 @@ public class World {
         return null;
     }*/
 
-
-    /*    Entity spawnEntity(Entity entity, Location location) {
-
-    }
-
-    Entity spawnEntity(Entity entity, Location location, Vector direction) {
-
-    }
-
-    Entity spawnEntity(Entity entity, Location location, Entity target) {
-
-    }
-
-    void removeEntity(Entity entity, ObservableList<Entity> liste) {
+   /* void removeEntity(Entity entity, ObservableList<Entity> liste) {
         liste.remove(entity);
-    }
-*/
+    }*/
+
     public void nextRound() {
-        for (Virus virus:getVirusList()){
+        for (Virus virus:getVirusList()) {
             virus.move();
-
-            for (Tower tower : getNodeList()) {
-                if (virus.isInRange(tower)) {
-                    virus.addTarget(tower);
-                }
-            }
-
             virus.act();
         }
-
-        for (Projectile projectile : getProjectileList())
-            projectile.move();
-
-        for(int i = getNodeList().size()-1; i>=0; i--){  // Browse the node list
-            for (int j = getVirusList().size()-1; j>=0; j--){  // Browse the virus list
-
-                // Virus j is in range of node i and not yet in its range list so we add it to the list
-                if (getNodeList().get(i).isInRange(getVirusList().get(j)) && !getNodeList().get(i).getInRangeVirus().contains(getVirusList().get(j))) {
-                    getNodeList().get(i).addRangedVirus(getVirusList().get(j));
-                }
-
-                // Virus j is not in range of node i and is in its range list so we remove it from the list
-                else if (!getNodeList().get(i).isInRange(getVirusList().get(j)) && getNodeList().get(i).getInRangeVirus().contains(getVirusList().get(j))) {
-                    getNodeList().get(i).delRangedVirus(getVirusList().get(j));
-                }
-
-                if (!getVirusList().get(j).isAlive())
-                    getVirusList().remove(j);
-            }
-
-            // Setting node target
-            if(!getNodeList().get(i).getInRangeVirus().isEmpty()) {
-                getNodeList().get(i).setTarget(getNodeList().get(i).getInRangeVirus().get(0));
-
-            }
-            else {
-                getNodeList().get(i).setTarget(null);
-            }
+        for (Tower tower:getNodeList()) {
+            tower.detection();
+            tower.act();
         }
+        for (Projectile projectile : getProjectileList()) {
+            projectile.move();
+        }
+
     }
-
-
-
-
 }
